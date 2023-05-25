4fc3c2de8f6b9a1100d040ea28ceb126558e887e
Anaka Norfleet <fleetster22@gmail.com>
2023-05-25 18:01:05 -0400

    added base.html and inheritance

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from recipes.views import show_recipe, recipe_list
+from recipes.views import show_recipe, recipe_list, create_recipe, edit_recipe
 
 urlpatterns = [
     # tells browser to navigate to recipes and then each recipe
-    path('recipes/<int:id>', show_recipe, name="show_recipe"),
-    path('recipes/', recipe_list, name="recipe_list")
+    path('<int:id>', show_recipe, name="show_recipe"),
+    path('', recipe_list, name="recipe_list"),
+    path('create/', create_recipe, name='create_recipe'),
+    path('<int:id>/edit/', edit_recipe, name='edit_recipe')
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -29,16 +29,37 @@ def recipe_list(request):
 
 # validates data entered in form and pushes data to db
 def create_recipe(request):
-    if request.method == "GET":
-        form = RecipeForm()
-    else:
+    if request.method == "POST":
         form = RecipeForm(request.POST)
         if form.is_valid():
             form.save()
             # tells browser which page to go to after data is saved
             return redirect("recipe_list")
 
+    else:
+        form = RecipeForm()
+
     context = {
-        "form": form
+        "form": form,
     }
-    return render(request, 'recipes/create.html')
+    return render(request, 'recipes/create.html', context)
+
+
+def edit_recipe(request, id):
+
+    recipe = get_object_or_404(Recipe, id=id)
+    if request.method == "POST":
+        form = RecipeForm(request.POST, instance=recipe)
+        if form.is_valid():
+            form.save()
+            # tells browser which page to go to after data is saved
+            return redirect("recipes/detail.html")
+
+    else:
+        form = RecipeForm(instance=recipe)
+
+    context = {
+        "recipe_object": recipe,
+        "form": form,
+    }
+    return render(request, 'recipes/edit.html', context)
