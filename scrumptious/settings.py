77f648b237c5eacdce52486aa96459cdc6061518
Anaka Norfleet <fleetster22@gmail.com>
2023-05-25 18:24:32 -0400

    moved sensitive data to .env file

--- scrumptious/settings.py
+++ scrumptious/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yc%gg(m(z(yo=d*i-87p76&)2&6so$tesu%q%w5ork*f(zpzwh'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
