ea1c5a1cf9a911101f53903b8e8db7e118d95bee
Anaka Norfleet <fleetster22@gmail.com>
2023-05-30 15:36:02 -0400

    added login functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from accounts.views import signup
+from accounts.views import signup, user_login
 
 urlpatterns = [
     # tells browser to navigate to recipes and then each recipe
     path('signup/', signup, name='signup'),
+    path('login/', user_login, name="user_login")
 ]
